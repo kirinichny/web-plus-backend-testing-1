9bd47794751e09fbb56c6e36ff9d62766c1c236c
Kirin <kirin-89@yandex.ru>
2025-05-07 03:40:49 +1000

    test: add unit tests for PostsService

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,22 +1,34 @@
-import { Post, PostsService } from './posts.service';
+import {Post, PostsService} from './posts.service';
 
 describe('PostsService', () => {
-  let postsService: PostsService;
-  const post: Omit<Post, 'id' | 'date'> = {
-    text: 'Mocked post',
-  };
+    let postsService: PostsService;
+    const post: Omit<Post, 'id' | 'date'> = {
+        text: 'Mocked post',
+    };
 
-  beforeEach(async () => {
-    postsService = new PostsService();
+    beforeEach(async () => {
+        postsService = new PostsService();
 
-    postsService.create({ text: 'Some pre-existing post' });
-  });
+        postsService.create({text: 'Some pre-existing post'});
+    });
 
-  it('should add a new post', () => {
-    // реализуйте тест-кейс
-  });
+    it('should add a new post', () => {
+        const initialLength = postsService['posts'].length;
 
-  it('should find a post', () => {
-    // реализуйте тест-кейс
-  });
+        const newPost = postsService.create(post);
+
+        expect(postsService['posts'].length).toBe(initialLength + 1);
+        expect(newPost.text).toBe(post.text);
+        expect(newPost.id).toBeDefined();
+        expect(newPost.date).toBeDefined();
+    });
+
+    it('should find a post', () => {
+        const postToFind = postsService.create(post);
+        const foundPost = postsService.find(postToFind.id);
+
+        expect(foundPost).not.toBeNull();
+        expect(foundPost?.id).toBe(postToFind.id);
+        expect(foundPost?.text).toBe(post.text);
+    });
 });
